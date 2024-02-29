5b8578c8be68b00b21757ee184b095180b83ecd9
zeze <77151276+zhengshunze@users.noreply.github.com>
2024-02-29 13:44:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@
 
 import logging, uvicorn
 from fastapi import FastAPI, Request, HTTPException
-
 from linebot import LineBotApi, WebhookHandler
-
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
@@ -21,42 +19,6 @@ line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET")) 
 
 
-	
-conversation = []
-
-class ChatGPT:  
-    
-
-    def __init__(self):
-        
-        self.messages = conversation
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
-
-
-
-    def get_response(self, user_input):
-        conversation.append({"role": "user", "content": user_input})
-        
-
-        response = openai.ChatCompletion.create(
-	            model=self.model,
-                messages = self.messages
-
-                )
-
-        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
-        
-        print("AI回答內容：")        
-        print(response['choices'][0]['message']['content'].strip())
-
-
-        
-        return response['choices'][0]['message']['content'].strip()
-	
-
-
-
-chatgpt = ChatGPT()
 
 app = FastAPI()
 # Line Bot config
@@ -81,16 +43,8 @@ def handling_message(event):
     #replyToken = event.reply_token
     
     if isinstance(event.message, TextMessage):
-        #messages = event.message.text
-        
+
         user_message = event.message.text
-
-
-        reply_msg = chatgpt.get_response(user_message)
-        
-        #echoMessages = TextSendMessage(text=messages)
-        #line_bot_api.reply_message(reply_token=replyToken, messages=echoMessages)
-
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=user_message))
 
 
